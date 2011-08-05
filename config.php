26b76f34e2b2093f6319fdf602fb2bd857828e85
alaguopenwave <alagesan.p@openwavecomp.in>
2011-08-05 16:02:20 +0530

    Config file update

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$dbms = 'mysqli';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'scholastic_forum';
+$dbuser = 'scholastic';
+$dbpasswd = 'NAM!@211';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+
+?>
